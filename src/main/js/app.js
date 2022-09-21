3bc21bb60937321a9a7e07c916400a91333c7044
Joshua Dane <joshrdane@gmail.com>
2022-09-21 17:47:36 -0400

    Project Skeleton

---
+++ src/main/js/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('react')
+)
