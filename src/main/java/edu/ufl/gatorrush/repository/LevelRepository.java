d24454a957356062eca9ac7035eb0d252dc3ae5f
Joshua Dane <joshrdane@gmail.com>
2022-10-28 08:42:23 -0400

    Implement Level Repository

---
+++ src/main/java/edu/ufl/gatorrush/repository/LevelRepository.java
@@ -0,0 +1,6 @@
+package edu.ufl.gatorrush.repository;
+
+import edu.ufl.gatorrush.model.Level;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LevelRepository extends CrudRepository<Level, Long> {}
