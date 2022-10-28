22240c2ca47433781812c547f0ac369c136b4550
Joshua Dane <joshrdane@gmail.com>
2022-10-28 07:52:11 -0400

    Implement Level

---
+++ src/main/java/edu/ufl/gatorrush/model/Level.java
@@ -0,0 +1,33 @@
+package edu.ufl.gatorrush.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Level {
+
+    /**
+     * Unique Identifier
+     */
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    /**
+     * List of Problems assigned to this Level
+     */
+    @ManyToMany
+    private List<Problem> problems;
+
+    public Long getId() {
+        return id;
+    }
+
+    public List<Problem> getProblems() {
+        return problems;
+    }
+
+    public void setProblems(List<Problem> problems) {
+        this.problems = problems;
+    }
+}
