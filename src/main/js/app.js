6deb9f134448ad39c3c0af49f46e14f7197279be
a-sommer <ava.r.sommer@gmail.com>
2022-11-01 13:30:45 -0400

    initial changes

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -8,6 +8,29 @@ class App extends React.Component {
     constructor(props) {
         super(props);
     }
+
+    render() {
+        return (
+            
+            <div>
+                <head>
+                    <link rel="stylesheet" href="/css/gamepage.css"/>
+                </head>
+                <div class="background-test">
+                    <div class="equation-container">
+                        <div class="equation-content">5 x 3 = ?</div>
+                    </div>
+                    
+                    <button class="btn-answer btn-answer-1">10</button>
+                    <button class="btn-answer btn-answer-2">200</button>
+                    <button class="btn-answer btn-answer-3">3</button>
+                    <button class="btn-answer btn-answer-4">40</button>
+                </div>
+            </div>
+                
+                
+        );
+    }
 }
 
 ReactDOM.render(
