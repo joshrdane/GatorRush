9da30952061db10d85c248c534e1b50acc32ab1b
a-sommer <ava.r.sommer@gmail.com>
2022-11-02 14:38:32 -0400

    Completed initial implementation of adventure mode page

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -2,12 +2,8 @@ import MainMenu from './mainmenu';
 
 'use strict';
 
-// import MainMenu from 'mainmenu';     breaks
-
 const React = require('react');
 const ReactDOM = require('react-dom');
-
-// import MainMenu from 'mainmenu';     breaks
 
 class App extends React.Component {
 
@@ -20,20 +16,25 @@ class App extends React.Component {
             
             <div>
                 <head>
-                    <link rel="stylesheet" href="/css/gamepage.css"/>
+                <link rel="stylesheet" href="/css/gamepage.css"/>
                 </head>
                 <div>
                     <MainMenu />
                 </div>
-                <div class="background-test">
+                <div class="background">
+                    <div class="container"/>
                     <div class="equation-container">
                         <div class="equation-content">5 x 3 = ?</div>
                     </div>
-                    
-                    <button class="btn-answer btn-answer-1">10</button>
-                    <button class="btn-answer btn-answer-2">200</button>
-                    <button class="btn-answer btn-answer-3">3</button>
-                    <button class="btn-answer btn-answer-4">40</button>
+                    <div class="container"/>
+                    <div class="container">
+                        <button class="btn-answer">10</button>
+                        <button class="btn-answer">2</button>
+                        <button class="btn-answer">300</button>
+                        <button class="btn-answer">4</button>
+                    </div> 
+                    <p class="attribution"><a href="https://www.freepik.com/free-vector/blank-landscape-nature-park-scene-with-swamp_9720494.htm#query=swamp&position=5&from_view=search&track=sph%22%3EImage">Image by brgfx</a> on Freepik</p>
+                    <p class="attribution-1"><a href="https://www.freepik.com/free-vector/game-ui-menu-interface-scrolls-parchments_31368897.htm#page=2&query=web%20game&position=11&from_view=search&track=sph%22%3EImage">Image by upklyak</a> on Freepik</p>
                 </div>
             </div>
                 
