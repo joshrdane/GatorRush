560593b2ea418458e8f5bd732cbbb08fc57bb8af
Joshua Dane <joshrdane@gmail.com>
2022-11-03 07:31:55 -0400

    Relocate Casual game mode into its own component

---
+++ src/main/js/casual.js
@@ -0,0 +1,40 @@
+import MainMenu from './mainmenu';
+
+'use strict';
+
+const React = require('react');
+
+class Casual extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <head>
+                    <link rel="stylesheet" href="/css/gamepage.css"/>
+                </head>
+                <div>
+                    <MainMenu />
+                </div>
+                <div class="background">
+                    <div class="container"/>
+                    <div class="equation-container">
+                        <div class="equation-content">5 x 3 = ?</div>
+                    </div>
+                    <div class="container"/>
+                    <div class="container">
+                        <button class="btn-answer">10</button>
+                        <button class="btn-answer">2</button>
+                        <button class="btn-answer">300</button>
+                        <button class="btn-answer">4</button>
+                    </div>
+                    <p class="attribution"><a href="https://www.freepik.com/free-vector/blank-landscape-nature-park-scene-with-swamp_9720494.htm#query=swamp&position=5&from_view=search&track=sph%22%3EImage">Image by brgfx</a> on Freepik</p>
+                    <p class="attribution-1"><a href="https://www.freepik.com/free-vector/game-ui-menu-interface-scrolls-parchments_31368897.htm#page=2&query=web%20game&position=11&from_view=search&track=sph%22%3EImage">Image by upklyak</a> on Freepik</p>
+                </div>
+            </div>
+        );
+    }
+}
