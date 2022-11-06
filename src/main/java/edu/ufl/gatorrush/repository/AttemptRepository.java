d7ce9b6e58ed9542a65898aa69b824300ca65d9b
Joshua Dane <joshrdane@gmail.com>
2022-11-06 15:22:57 -0500

    Add REST repository for Attempt

---
+++ src/main/java/edu/ufl/gatorrush/repository/AttemptRepository.java
@@ -0,0 +1,6 @@
+package edu.ufl.gatorrush.repository;
+
+import edu.ufl.gatorrush.model.Attempt;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AttemptRepository extends CrudRepository<Attempt, Long> {}
