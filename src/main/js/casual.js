cded392639b15e47df1839a51e1e136541e9570b
Joshua Dane <joshrdane@gmail.com>
2022-11-08 09:22:10 -0500

    Integrate changes to backend and perform automatic casual progression

--- src/main/js/casual.js
+++ src/main/js/casual.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const React = require('react');
-const client = require('./client');
 
 class Casual extends React.Component {
 
@@ -11,46 +10,103 @@ class Casual extends React.Component {
             loading: true,
             level: {
                 id: 0,
-                name: "0",
+                name: '0',
                 problems: []
             },
             problem: {
+                id: 0,
                 leftOperand: 0,
                 operator: '?',
-                rightOperand: 0
+                rightOperand: 0,
+                options: []
             },
-            wrong: []
+            history: []
         }
         this.handleSelect = this.handleSelect.bind(this);
     }
 
     handleSelect(e) {
-        const selection = e.target.value;
-        //
-        if (selection === this.state.problem.result) {
-        } else {
-            // Place problem back into rotation
-            const probs = this.state.level.problems;
-            this.setState({
-                problem: probs.at(0),
-                level: {
-                    problems: probs.slice(1)
+        let current = this.state.problem;
+        current.response = parseInt(e.target.innerHTML);
+        // Create a 'new' history instance
+        let newHistory = this.state.history.concat(current);
+        /* Post to attempts */
+        // Temporary logic until we get authentication
+        let user = null;
+        if (user) {
+            // Saves attempt to database
+            fetch(`http://localhost:8080/attempt/challenge?user=${user}&problem=${this.state.problem.id}&response=${current.response}`, {method: 'post'});
+        }
+        // Check local history for completion criteria
+        let correct = 0;
+        // Ensure at least 10 problems have been attempted
+        if (newHistory.length >= 10) {
+            // Get last 10 problems attempted
+            for (let problem in newHistory.slice(-10)) {
+                if (problem.response === problem.result) {
+                    correct++;
                 }
-            })
+            }
+        }
+        if (correct >= 7) {
+            // TODO: Add error handling for 400 & 400 errors
+            // TODO: Add handle final level completion error 418
+            fetch(`http://localhost:8080/level?${user === null ? `id=${this.state.level.id}` : `user=${user}`}`, {method: 'post'})
+                .then(response => response.json())
+                .then(response => {
+                    this.setState({
+                        // Blanket drop response level JSON into level
+                        level: response
+                    });
+                    this.setState({
+                        // Set problem to the first problem in level
+                        problem: this.state.level.problems.at(0),
+                        level: {
+                            // The next two lines are required to not nullify the existing values
+                            id: this.state.level.id,
+                            name: this.state.level.name,
+                            // Set remaining problems to exclude removed problem
+                            problems: this.state.level.problems.slice(1)
+                        },
+                        // Reset history for new level
+                        history: []
+                    });
+                });
+            // TODO: Show history? note: use newHistory
+        } else {
+            this.setState({
+                problem: this.state.level.problems.at(0),
+                level: {
+                    // The next two lines are required to not nullify the existing values
+                    id: this.state.level.id,
+                    name: this.state.level.name,
+                    // Set remaining problems to exclude removed problem and add previous problem
+                    problems: this.state.level.problems.slice(1).concat(this.state.problem)
+                },
+                history: newHistory
+            });
         }
     }
 
     componentDidMount() {
-        client({method: 'GET', path: `/api/levels/4/problems`}).done(response => {
-            const probs = response.entity._embedded.problems;
-            this.setState({
-                loading: false,
-                problem: probs.at(0),
-                level: {
-                    problems: probs.slice(1)
-                }
+        fetch('http://localhost:8080/level?id=4')
+            .then(response => response.json())
+            .then(response => {
+                this.setState({
+                    level: response
+                });
+                this.setState({
+                    loading: false,
+                    problem: this.state.level.problems.at(0),
+                    level: {
+                        // The next two lines are required to not nullify the existing values
+                        id: this.state.level.id,
+                        name: this.state.level.name,
+                        problems: this.state.level.problems.slice(1)
+                    }
+                });
+                console.log(response);
             });
-        });
     }
 
     render() {
@@ -73,7 +129,14 @@ class Casual extends React.Component {
                     <div className="container">
                         {
                             this.state.problem.options.map((option, index) => {
-                                return <button key={Math.random() * 10000 + option} className="btn-answer" onClick={this.handleSelect}>{this.state.problem.options.at(index)}</button>
+                                return (
+                                    <button
+                                        key={`${this.state.level.id}-${this.state.problem.id}-${index}-${option}-${this.state.history.length}`}
+                                        className="btn-answer"
+                                        onClick={this.handleSelect}>
+                                        {this.state.problem.options.at(index)}
+                                    </button>
+                                )
                             })
                         }
                     </div>
