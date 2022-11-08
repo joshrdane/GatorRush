59f4ffb4bc3748f6c189b70d678c7857d889ad12
Joshua Dane <joshrdane@gmail.com>
2022-11-08 09:14:35 -0500

    Add Data Transfer Objects (DTOs) for data cleaning/presentation/security

---
+++ src/main/java/edu/ufl/gatorrush/model/dto/UserDto.java
@@ -0,0 +1,19 @@
+package edu.ufl.gatorrush.model.dto;
+
+import edu.ufl.gatorrush.model.User;
+
+public class UserDto {
+    private User user;
+
+    public UserDto(User user) {
+        this.user = user;
+    }
+
+    public Long getId() {
+        return user.getId();
+    }
+
+    public String getUsername() {
+        return user.getUsername();
+    }
+}
