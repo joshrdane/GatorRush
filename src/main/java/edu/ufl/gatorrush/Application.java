2581fa45a5a023d1c8e05bb34a28e2ed91acab55
a-sommer <ava.r.sommer@gmail.com>
2022-11-18 16:08:55 -0500

    Reverted algorithm for problem generation to match previous. Temporarily adjusted for testing purposes

--- src/main/java/edu/ufl/gatorrush/Application.java
+++ src/main/java/edu/ufl/gatorrush/Application.java
@@ -44,9 +44,9 @@ public class Application {
         Arrays.stream(new Character[] {'+', '-', 'x', '/'}).forEach(character -> pool.put(character, new ArrayList<>()));
         // Define parameters for generation
         int addition = 1;
-        int subtraction = addition + 1;
-        int multiplication = subtraction + 1;
-        int division = multiplication + 1;
+        int subtraction = addition + 5;
+        int multiplication = subtraction + 5;
+        int division = multiplication + 5;
         // Generate pool of problems
         for (int left = 0; left < 100; left++) {
             for (int right = 0; right < 100; right++) {
