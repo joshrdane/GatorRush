c1c7d095fbb7ccbc1804a45aff0ba08c7d2b355b
Pedro <pmlima99@gmail.com>
2022-11-20 15:33:36 -0500

    Updating immediate feedback feature to a popup

--- src/main/js/components/immediateFeedback.js
+++ src/main/js/components/immediateFeedback.js
@@ -1,40 +1,73 @@
-
-import React from "react";
-
-
-
-const ImmediateFeedback = (props) => {
+import React from 'react'
+import {useEffect} from 'react';
 
 
 
-    const {feedback} = props;
 
-    let c = 'green';
+function ImmediateFeedback(props){
+    let setTrigger = props.trigger;
+    let isCorrect = props.isCorrect;
 
-    if( feedback === "Not quite. Try again." ) {
-        c = 'red';
+    let bgColor = '';
+    let color = '';
+
+    let feedback = []
+
+
+    let feedbackIndex = Math.floor(Math.random() * 4);
+
+    // will change the style and feedback message depending if the user selected the correct answer or not
+    if(isCorrect){
+        bgColor = 'rgb(25 231 110)';
+        color = '#bd781d';
+        feedback = ["Great job!", "You got it!", "Awesome!", "You did it!"]
+    }
+    else{
+        color = 'rgb(89 14 14)';
+        bgColor = 'rgb(226 100 75)';
+        feedback = ["Try again!", "You're almost there!", "So close!", "You can do this!"]
+    }
+
+    const popupStyle = {
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100vh',
+        backgroundColor: 'rgba(0, 0, 0, 0.2)',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: 'black',
+        textAlign: 'center'
+
+
+    }
+
+    const popupInner = {
+        position: 'relative',
+        padding: '32px',
+        width: '100%',
+        // maxWidth: '640px',
+        maxWidth: '340px',
+        backgroundColor: bgColor,
+        color: color,
+        borderRadius: '2rem'
+
     }
 
 
 
-    const feedbackStyle = {
-        textAlign: 'center',
-        fontFamily: 'fantasy',
-        fontSize: '16px',
-        display: 'contents',
-        color: c,
-    }
+    return(setTrigger) ? (
+        <div style={popupStyle}>
+            <div style={popupInner}>
+                <h3>{feedback[feedbackIndex]}</h3>
+            </div>
 
 
-    const labelStyle = {
-        fontSize: '1rem'
-    }
-
-    return (
-        <div style={feedbackStyle}>
-            {feedback}
         </div>
-    );
-};
+    ) : "";
+}
+
 
 export default ImmediateFeedback;
