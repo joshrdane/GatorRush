6766967262dcda9fdd989d092936b6ca9af92ca2
Joshua Dane <joshrdane@gmail.com>
2022-11-28 12:53:19 -0500

    Add ignore-case logic to user repository

--- src/main/java/edu/ufl/gatorrush/repository/UserRepository.java
+++ src/main/java/edu/ufl/gatorrush/repository/UserRepository.java
@@ -3,8 +3,10 @@ package edu.ufl.gatorrush.repository;
 import edu.ufl.gatorrush.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+    Optional<User> findByUsernameIgnoreCase(String username);
 
     Boolean existsByEmail(String email);
 
