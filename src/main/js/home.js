27afb6bcf50f262f3792f784add1622589a37e28
Joshua Dane <joshrdane@gmail.com>
2022-11-29 10:50:52 -0500

    Settled merge conflicts

--- src/main/js/home.js
+++ src/main/js/home.js
@@ -9,13 +9,26 @@ class Home extends React.Component {
 
     constructor(props) {
         super(props);
-        this.handlePageChange = this.handlePageChange.bind(this);
-        this.handleLogin = this.handleLogin.bind(this);
-        this.handleCreateAcc = this.handleCreateAcc.bind(this);
         this.state = {
+            username: "",
+            password: "",
+            rememberMe: false,
             trigger: false
         }
-        // this.trigger = false;
+        this.handleChange = this.handleChange.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
+        this.handlePageChange = this.handlePageChange.bind(this);
+        this.handleCreateAcc = this.handleCreateAcc.bind(this);
+    }
+
+    handleChange(e) {
+        const target = e.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
     }
 
     handleLogin(e, username, password) {
@@ -41,47 +54,37 @@ class Home extends React.Component {
 
 
     render() {
-
+        const { username, password, rememberMe } = this.state;
         return (
             <div className="background">
-
                 <div>
-                <div className="container">
-                    <div className="title"><p>Gator Rush</p></div>
-                </div>
-
-                <div className="container">
-                    <img className="alligator-homepage" src="/images/Gator_TransparentBG.png"/>
-
-
-
-                    <div className="login-container">
-
-                        <div className="username-input">
-                            <input type="text" id="username-input" name="username-input" maxLength="15"/>
-                        </div>
-                        <div className="password-input">
-                            <input type="password" id="password-input" name="password-input"/>
-                        </div>
-                        <div className="remember-me">
-                            <input type="checkbox" id="remember-me" name="remember-me"/>
-                        </div>
+                    <div className="container">
+                        <div className="title"><p>Gator Rush</p></div>
                     </div>
-
-
-
-
-
-
+                    <form className="container">
+                        <img className="alligator-homepage" src="/images/Gator_TransparentBG.png"/>
+                        <div className="login-container">
+                            <div className="username-input">
+                                <input type="text" id="username-input" name="username" maxLength="15" value={username} onChange={this.handleChange}/>
+                            </div>
+                            <div className="password-input">
+                                <input type="password" id="password-input" name="password" value={password} onChange={this.handleChange}/>
+                            </div>
+                            <div className="remember-me">
+                                <input type="checkbox" id="remember-me" name="rememberMe" value={rememberMe} onChange={this.handleChange}/>
+                            </div>
+                            <div>
+                                <button type="submit" id="login-button" onSubmit={this.handleLogin}></button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
 
                 <div className="container">
                     <button data-page={"create-account"} className="btn-sign-up" onClick={this.handleCreateAcc}>Create an Account
                     </button>
-
-
                 </div>
-
+                
                 <div className="container">
                     <div className="game_description-container">
                         <div className="game_description-content">
@@ -90,17 +93,13 @@ class Home extends React.Component {
                         </div>
                     </div>
                 </div>
-
-
-
-
+                
                 <p className="attribution"><a
                     href="https://www.freepik.com/free-vector/blank-landscape-nature-park-scene-with-swamp_9720494.htm#query=swamp&position=5&from_view=search&track=sph%22%3EImage">Image
                     by brgfx</a> on Freepik</p>
                 <p className="attribution-1"><a
                     href="https://www.freepik.com/free-vector/game-ui-menu-interface-scrolls-parchments_31368897.htm#page=2&query=web%20game&position=11&from_view=search&track=sph%22%3EImage">Image
                     by upklyak</a> on Freepik</p>
-                </div>
 
                 <CreateAccount trigger = {this.state.trigger} handleLogin={this.handleLogin} handleTrigger={this.handleCreateAcc} handlePageChange={this.handlePageChange}/>
 
