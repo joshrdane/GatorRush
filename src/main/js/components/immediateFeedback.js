5accbdac7ff6931a046958a6edb3af13538e9aca
Joshua Dane <joshrdane@gmail.com>
2022-12-02 14:29:26 -0500

    Remove redundant assignment and reorder

--- src/main/js/components/immediateFeedback.js
+++ src/main/js/components/immediateFeedback.js
@@ -4,12 +4,11 @@ function ImmediateFeedback(props){
     let setTrigger = props.trigger;
     let isCorrect = props.isCorrect;
 
-    let bgColor = '';
-    let color = '';
-
-    let feedback = []
-
     let feedbackIndex = Math.floor(Math.random() * 4);
+
+    let bgColor;
+    let color;
+    let feedback;
 
     // will change the style and feedback message depending if the user selected the correct answer or not
     if(isCorrect){
